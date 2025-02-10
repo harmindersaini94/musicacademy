e07f64fa5f5d0173e480495397ea8223c7d7a9e2
Harminder Saini <harminder_saini94@outlook.com>
2025-02-09 16:57:01 -0800

    Hero Section Completed

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,4 +1,24 @@
+import { taintObjectReference } from "next/dist/server/app-render/entry-base";
 import type { Config } from "tailwindcss";
+
+import svgToDataUri from "mini-svg-data-uri";
+import colors from "tailwindcss/colors";
+
+const {
+  default: flattenColorPalette,
+} = require("tailwindcss/lib/util/flattenColorPalette");
+
+// Plugin to add each Tailwind color as a global CSS variable
+function addVariablesForColors({ addBase, theme }: any) {
+  const allColors = flattenColorPalette(theme('colors'));
+  const newVars = Object.fromEntries(
+    Object.entries(allColors).map(([key, value]) => [`--${key}`, value])
+  );
+
+  addBase({
+    ':root': newVars,
+  });
+}
 
 export default {
   content: [
@@ -6,13 +26,29 @@ export default {
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
+  darkMode: "class",
   theme: {
     extend: {
+      animation: {
+        spotlight: "spotlight 2s ease .75s 1 forwards",
+      },
+      keyframes: {
+        spotlight: {
+          "0%": {
+            opacity: "0",
+            transform: "translate(-72%, -62%) scale(0.5)",
+          },
+          "100%": {
+            opacity: "1",
+            transform: "translate(-50%,-40%) scale(1)",
+          },
+        },
+      },
       colors: {
         background: "var(--background)",
         foreground: "var(--foreground)",
       },
     },
   },
-  plugins: [],
+  plugins: [addVariablesForColors],
 } satisfies Config;
